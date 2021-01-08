db2a0496e8e3338e51ea67e064913943ba114317
Cristel Capetillo <cristel.capetillo@outlook.com>
2021-01-08 12:40:20 +0100

    Adds MainMenu & buttons working on it

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    private int sceneToLoad;
+    private int currentSceneIndex;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+        sceneToLoad = PlayerPrefs.GetInt("SavedScene");
+        if (sceneToLoad != 0)
+            SceneManager.LoadScene(sceneToLoad);
+        else
+        {
+            return;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Over");
+    }
+
+    public void GoToMainMenu()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        PlayerPrefs.SetInt("SavedScene", currentSceneIndex);
+        SceneManager.LoadScene(0);
+    }
+    
+}
